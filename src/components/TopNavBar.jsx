6b3ef8b9a9431d236cd39775cb30099eae05599e
Oxarellys <oxaurbaneja@gmail.com>
2026-05-08 09:56:28 -0400

    feat: Delete 'Nosotros' page and update navigation links

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -14,7 +14,6 @@ export default function TopNavBar() {
         <div className="hidden md:flex items-center gap-8">
           <Link className="text-slate-600 font-medium hover:text-[#F28705] transition-all duration-200 text-[16px] hover:-translate-y-0.5 inline-block" href="/">Inicio</Link>
           <Link className="text-slate-600 font-medium hover:text-[#F28705] transition-all duration-200 text-[16px] hover:-translate-y-0.5 inline-block" href="/catalogo">Catálogo</Link>
-          <Link className="text-slate-600 font-medium hover:text-[#F28705] transition-all duration-200 text-[16px] hover:-translate-y-0.5 inline-block" href="/nosotros">Nosotros</Link>
         </div>
         <button className="bg-[#F28705] text-white px-6 py-3 rounded-xl font-button active:scale-95 transition-all duration-300 shadow-lg shadow-orange-500/20 hover:shadow-orange-500/40 hover:-translate-y-1">
           Comprar Ahora
